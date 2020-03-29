c956fe09757d44b6f13ced3f74cf0d4b5958b1cc
Irving Gonzalez <ialexis93@gmail.com>
2020-03-29 01:21:42 -0600

    init project

---
+++ app/src/main/java/com/sysgon/svgps/data/LoginDataSource.java
@@ -0,0 +1,36 @@
+package com.sysgon.svgps.data;
+
+import android.util.Log;
+
+import com.sysgon.svgps.data.model.LoggedInUser;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * Class that handles authentication w/ login credentials and retrieves user information.
+ */
+public class LoginDataSource {
+
+    public Result<LoggedInUser> login(String username, String password) {
+
+        try {
+            // TODO: handle loggedInUser authentication
+            LoggedInUser fakeUser =
+                    new LoggedInUser(
+                            java.util.UUID.randomUUID().toString(),
+                            "Jane Doe");
+            return new Result.Success<>(fakeUser);
+        } catch (Exception e) {
+            return new Result.Error(new IOException("Error logging in", e));
+        }
+    }
+
+
+    public void logout() {
+        // TODO: revoke authentication
+    }
+}
